91721f88b6b891f65a73f14385ab2a3548bafa7b
User <User@TOSH-Lena>
2017-03-22 17:57:39 +0000

    commented guard subclasses

--- Project_1/src/dkeep/logic/Drunken.java
+++ Project_1/src/dkeep/logic/Drunken.java
@@ -2,9 +2,21 @@ package dkeep.logic;
 
 import java.util.Random;
 
+/**  
+* Drunken.java - A subclass of Guard to represent a personality.
+*/
 public class Drunken extends Guarda {
 	Random rd = new Random();
 	boolean dir = true;
+	
+	/**  
+	* Constructor of the class Suspicious.
+	* @param x X position of the Entidade.
+	* @param y Y position of the Entidade.
+	* @param tag The simple graphic representation of the Entidade.
+	* @param mov Array that has the movement information of the guarda.
+	* @see Guarda
+	*/ 
 	public Drunken(int x, int y, char tag, char[] mov) {
 		super(x, y, tag, mov);
 	}
@@ -13,6 +25,13 @@ public class Drunken extends Guarda {
 		return dir;
 	}
 
+	/**  
+	* Method responsible for moving the guard according to its personality.
+	* @param direction char that represents the direction to move.
+	* @param b Board in which to move the Guarda.
+	* @see Guarda
+	* @see ComportamentoGuarda 
+	*/
 	public void move(char direction, Board b) {
 		int i = rd.nextInt(10);
 		if (i < 3) {
